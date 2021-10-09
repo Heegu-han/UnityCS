f4e57c408e2b84a8e6a589acdae41efa000cf603
Kyle Han <hanheegu7@gmail.com>
2021-10-09 12:42:06 +0900

    multiproject

---
+++ secondproject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secondproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("저는 한희구 입니다.");
+        }
+    }
+}
