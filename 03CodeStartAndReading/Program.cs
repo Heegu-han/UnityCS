cb405c22a0869b1eb8d8ba905e39e3cafc2910a3
Kyle Han <hanheegu7@gmail.com>
2021-10-11 15:14:02 +0900

    211011-3

--- 03CodeStartAndReading/Program.cs
+++ 03CodeStartAndReading/Program.cs
@@ -8,8 +8,11 @@ namespace _03CodeStartAndReading
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // 프로그램은 여기서 부터 읽는다
         {
+            Console.WriteLine("하하"); // F10 눌러서 디버깅
+            Console.WriteLine("호호");
+            Console.WriteLine("히히");
         }
     }
 }
