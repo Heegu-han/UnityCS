17bf74d3d64d5e55e2325d46e15984b23c0cb849
Kyle Han <hanheegu7@gmail.com>
2021-10-11 14:31:52 +0900

    211011-1

---
+++ 01Class/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// C#은 굉장히 객체지향적
+// 객체지향 프로그래밍의 가장 핵심적인 내용을 설명하고 있는것.
+class Monster
+{
+    // 내부의 내용
+}
+
+class Pet
+{
+    // 내부의 내용
+}
