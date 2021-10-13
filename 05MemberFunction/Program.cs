99d942c72213ddb545c784beaf4dd0408190f570
Kyle Han <hanheegu7@gmail.com>
2021-10-13 15:28:22 +0900

    211013-3

---
+++ 05MemberFunction/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 행동
+// 움직인다.
+// 공격한다.
+// 대화한다.
+
+class Player
+{
+
+    int Atk;
+    int HP;
+
+}
