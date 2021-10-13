9139753838db1900b3ca8a2688f4b9959ac35168
Kyle Han <hanheegu7@gmail.com>
2021-10-13 15:47:09 +0900

    211013-5

---
+++ 06LocalVar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Player
+{
+    int Atk;
+    int HP;
+    
+    void Fight()
+    {
+        Console.WriteLine("플레이어가 싸운다");
+    }
+}
+namespace _06LocalVar
+{
+    // c#은 고지식한 객체지향 언어.
+    // 프로그램의 시작조차도 클래스 안에 묶어놔야한다. 
+    class Program
+    {
+        // 시작용 함수
+        static void Main(string[] args)
+        {
+            Player
+            // Console.WriteLine("지역변수를 배우기");
+        }
+    }
+}
