668e6d38eb36e1a802b7eae1644495c500649a1e
Kyle Han <hanheegu7@gmail.com>
2021-10-13 15:54:18 +0900

    211013-6

--- 06LocalVar/Program.cs
+++ 06LocalVar/Program.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 
 class Player
 {
+    
     int Atk;
     int HP;
     
     void Fight()
     {
+        int Damage = 0;
         Console.WriteLine("플레이어가 싸운다");
     }
 }
@@ -23,7 +25,18 @@ namespace _06LocalVar
         // 시작용 함수
         static void Main(string[] args)
         {
-            Player
+            // 클래스 안에있는 얘들을 맴버변수
+            // 함수안에 있는 얘들을 지역변수라고 한다.
+            int Atk = 0;
+            Atk = 50;
+
+            // 클래스는 설계도
+            // Player의 설계대로 플레이어를 만드는데
+            // 그 이름을 NewPlayer라고 해서 만든다.
+
+            Player NewPlayer1 = new Player();
+            Player NewPlayer2 = new Player();
+            
             // Console.WriteLine("지역변수를 배우기");
         }
     }
