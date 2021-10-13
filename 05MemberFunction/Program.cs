31b325cdd3c63e2b9f3add3da544488de4f586d6
Kyle Han <hanheegu7@gmail.com>
2021-10-13 15:38:04 +0900

    211013-4

--- 05MemberFunction/Program.cs
+++ 05MemberFunction/Program.cs
@@ -4,15 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// 행동
-// 움직인다.
-// 공격한다.
-// 대화한다.
 
 class Player
 {
-
+    // 명사 만으로는 부족
+    int Gold;
     int Atk;
     int HP;
+    // 행동이 필요
+    // 지형에서 움직인다.
+    // 몬스터를 공격한다.
+    // NPC와 대화한다.
+    // 이런것들을 표현하는것이 함수.
+    /*리턴값*/void /*리턴값*/ Move /*인자값*/()
+    {// <-함수 시작
 
+    }// <-함수 끝
+    void Fight()
+    {
+
+    }
+    
+    void Talk()
+    {
+
+    }
 }
