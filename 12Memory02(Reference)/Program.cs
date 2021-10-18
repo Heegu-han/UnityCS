ce2c807952af020541d2a68ca1c563a6068d9a83
HeeguHan <hanheegu7@gmail.com>
2021-10-18 16:14:49 +0900

    211018-5

--- 12Memory02(Reference)/Program.cs
+++ 12Memory02(Reference)/Program.cs
@@ -28,11 +28,20 @@ namespace _12Memory02_Reference_
     {
         static void Main(string[] args)
         {
+            // 값형과 레퍼런스 형을 구분할줄 알아야한다.
+            // 기본자료형을 선언만 하면 그녀석들은 값형이다.
+            
+            // 자기가 만든 클래스를 new 클래스명();
+            // 해서 만든것을 그 클래스명에 받으면
+            // 이런 녀석을 레퍼런스형이라고 한다.
+
+
             // HP 100 / AT 10
             // 이 래퍼런스라고 하는 녀석은
             // 힙에 생성된 어떠한 메모리의 위치를 가르킴
             Monster NewMonster = new Monster();
-            Player NewPlayer = new Player();
+            Player NewPlayer = new Player(); // 힙 영역에 생성됨
+
 
             // 우리가 전에 배웠던대로라면
             // 플레이어가 자신의 공격력으로 때렸지만
---
+++ 13StaticVar/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// static
+
+class Player
+{
+    // 일반적인 맴버변수아 다른점
+    // static을 붙이게 되면
+    // 이런 경우 정적 맴버변수라고 한다.
+    // 정적 맴버변수는 일반적인 맴버변수와 다르게
+    // 객체화를 사용하지않고 사용할수 있다.
+    // 클래스명으로만으로 사용가능하다.
+    public static int PlayerCount = 0;
+
+
+    public int AT=10;
+    public int HP=100;
+    // 클래스 내부 안에서는 사용가능하다.
+    public void Setting(int _AT, int _HP)
+    {
+
+        AT = _AT;
+        HP = _HP;
+    }
+}
+namespace _13StaticVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player NewPlayer1 = new Player();
+            Player.PlayerCount = 1;
+            Player NewPlayer2 = new Player();
+            Player.PlayerCount = 2;
+            Player NewPlayer3 = new Player();
+            Player.PlayerCount = 3;
+
+            // 이런식으로 객체화를 전혀 하지 않고 사용할수가 있고
+            // 정적 맴버변수라고 부르고
+            // 이런 정적 맴버변수는 데이터 영역에 들어가게 된다.
+
+
+            // 이 맴버변수라고 하는 녀석은 각각 존재한다.
+            NewPlayer1.Setting(10, 100);
+            NewPlayer2.Setting(20, 50);
+            NewPlayer3.Setting(100, 500);
+
+            // 모든 객체가 값을 공유할 필요가 있는 데이터를
+            // 정의하고데 사용한다.
+        }
+    }
+}
