79863f667e66bb1e0549cf74312514944a17dfec
HeeguHan <hanheegu7@gmail.com>
2021-10-18 14:09:51 +0900

    211018-2

--- 08FuncEx/Program.cs
+++ 08FuncEx/Program.cs
@@ -7,8 +7,30 @@ using System.Threading.Tasks;
 
 class Player
 {
-    public int Hp;
-    int Atk;
+    private int HP = 10;
+    private int Atk = 100;
+    private int LV = 10;
+    public int GetLv()
+    {
+        //LV = 10000;
+        return LV;
+        LV = 10000;
+    }
+    public void LVUP()
+    {
+        HP = 1000;
+        Atk = 1000;
+    }
+    public void SetHP(int _HP)
+    {
+        HP = _HP;
+    }
+    public void Damage(int _Dmg, int _SubDmg)
+    {
+        HP = HP - _Dmg;
+        HP = HP - _SubDmg;
+    }
+
 }
 namespace _08FuncEx
 {
@@ -17,7 +39,11 @@ namespace _08FuncEx
         static void Main(string[] args)
         {
             Player NewPlayer = new Player();
-            NewPlayer.Hp
+            // NewPlayer.Hp = 0;
+            NewPlayer.SetHP(100);
+            NewPlayer.LVUP();
+
+            Console.WriteLine(NewPlayer.GetLv());
         }
     }
 }
