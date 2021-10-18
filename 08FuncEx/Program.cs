aee6944d33e26ce4581911ee6bf5806c667f10b3
HeeguHan <hanheegu7@gmail.com>
2021-10-18 11:54:03 +0900

    211018-1

---
+++ 08FuncEx/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Player
+{
+    public int Hp;
+    int Atk;
+}
+namespace _08FuncEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player NewPlayer = new Player();
+            NewPlayer.Hp
+        }
+    }
+}
