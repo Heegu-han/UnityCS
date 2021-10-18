fe8319d8ece6ea29d87a007674a4cf3d2bae42cd
HeeguHan <hanheegu7@gmail.com>
2021-10-18 15:37:32 +0900

    211018-4

--- 11Memory00(Value)/Program.cs
+++ 11Memory00(Value)/Program.cs
@@ -11,7 +11,9 @@ class Player
 
     public void Test(int _Dmg)
     {
-        _Dmg = 1000;
+        // 이 안에 지역변수에서 벌어지는 일은
+        // 이 안에서만 일어남
+        _Dmg = 1002342;
     }
 }
 namespace _11Memory00_Value_
@@ -21,7 +23,7 @@ namespace _11Memory00_Value_
         static void Main(string[] args)
         {
             // 함수의 인자값
-            int Value = 0;
+            int Value = 10;
 
             Player Newplayer = new Player();
             Newplayer.Test(Value);
---
+++ 12Memory02(Reference)/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Player
+{
+    public int AT = 10;
+    public int HP = 100;
+    public void ATT(Monster _Monster)
+    {
+        _Monster.HP -= AT;
+    }
+}
+class Monster
+{
+    public int AT = 10;
+    public int HP = 100;
+    public void ATT(Player _Player)
+    {
+        _Player.HP -= AT;
+    }
+}
+namespace _12Memory02_Reference_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // HP 100 / AT 10
+            // 이 래퍼런스라고 하는 녀석은
+            // 힙에 생성된 어떠한 메모리의 위치를 가르킴
+            Monster NewMonster = new Monster();
+            Player NewPlayer = new Player();
+
+            // 우리가 전에 배웠던대로라면
+            // 플레이어가 자신의 공격력으로 때렸지만
+            // 적용이 될리가 없다.
+            NewPlayer.ATT(NewMonster);
+            NewPlayer.ATT(NewMonster);
+            NewPlayer.ATT(NewMonster);
+            NewMonster.ATT(NewPlayer);
+            // 플레이어의 hp가 깍인것이 보인다.
+        }
+    }
+}
