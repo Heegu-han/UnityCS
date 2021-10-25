fbf33245bd620aa22015bf21bd0f6cde441b9f37
Kyle Han <hanheegu7@gmail.com>
2021-10-25 17:06:54 +0900

    211025-4

---
+++ winform2/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace winform2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tboxResult = new System.Windows.Forms.TextBox();
+            this.tboxNum1 = new System.Windows.Forms.TextBox();
+            this.tboxNum2 = new System.Windows.Forms.TextBox();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMulti = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tboxResult
+            // 
+            this.tboxResult.Enabled = false;
+            this.tboxResult.Location = new System.Drawing.Point(39, 32);
+            this.tboxResult.Name = "tboxResult";
+            this.tboxResult.Size = new System.Drawing.Size(203, 21);
+            this.tboxResult.TabIndex = 0;
+            // 
+            // tboxNum1
+            // 
+            this.tboxNum1.Location = new System.Drawing.Point(39, 59);
+            this.tboxNum1.Name = "tboxNum1";
+            this.tboxNum1.Size = new System.Drawing.Size(100, 21);
+            this.tboxNum1.TabIndex = 1;
+            // 
+            // tboxNum2
+            // 
+            this.tboxNum2.Location = new System.Drawing.Point(145, 59);
+            this.tboxNum2.Name = "tboxNum2";
+            this.tboxNum2.Size = new System.Drawing.Size(97, 21);
+            this.tboxNum2.TabIndex = 2;
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(39, 87);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(43, 33);
+            this.btnPlus.TabIndex = 3;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMulti
+            // 
+            this.btnMulti.Location = new System.Drawing.Point(150, 87);
+            this.btnMulti.Name = "btnMulti";
+            this.btnMulti.Size = new System.Drawing.Size(43, 33);
+            this.btnMulti.TabIndex = 5;
+            this.btnMulti.Text = "*";
+            this.btnMulti.UseVisualStyleBackColor = true;
+            this.btnMulti.Click += new System.EventHandler(this.btnMulti_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(199, 87);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(43, 33);
+            this.btnDiv.TabIndex = 6;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(99, 87);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(40, 33);
+            this.btnMinus.TabIndex = 7;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 204);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMulti);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.tboxNum2);
+            this.Controls.Add(this.tboxNum1);
+            this.Controls.Add(this.tboxResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tboxResult;
+        private System.Windows.Forms.TextBox tboxNum1;
+        private System.Windows.Forms.TextBox tboxNum2;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnMinus;
+    }
+}
+
